6fc7227aa77b7c2f7bc28a9203382b005afc991e
FranciscoCorreia28 <francisco.correia1804@gmail.com>
2022-03-11 08:08:10 +0100

    feat: footer add

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Navbar from './Navbar';
 import BookContextProvider from './context/BookContext';
 import BookList from './BookList';
+import Footer from './Footer';
 import '../assets/css/style.css';
 import NewBookForm from './BookForm';
 
@@ -14,6 +15,7 @@ export default function () {
                 <Navbar />
                 <BookList />
                 <NewBookForm />
+                <Footer/>
             </BookContextProvider>
         </div>
     );
---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Footer(params) {
+    return (
+        <div className="footer">
+           <p>Made with love by Francisco Correia</p>
+        </div>
+    ) 
+};
